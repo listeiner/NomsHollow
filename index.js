dc4037df35c5563da6a18e06f2d67e906e611904
Lindsey Steiner <105761115+Razetto@users.noreply.github.com>
2023-06-16 15:47:51 -0400

    Created base web page for Nom's Hollow.

---
+++ index.js
@@ -0,0 +1,18 @@
+const bios = document.querySelector(".sprite-container");
+
+const renderSingleElement = (element) => {
+    // extract the info
+    const {name, spriteLink} = element;
+    return `<div class="sprite-element"><img src=${spriteLink} alt=${name}></img></div>`;
+}
+
+const renderElements = (elementsArray) => {
+    console.log(elementsArray);
+    const transformation1 = elementsArray.map(e => renderSingleElement(e));
+    console.log(transformation1);
+    const transformation2 = transformation1.join("");
+    console.log(transformation2);
+    return transformation2;
+}
+
+bios.innerHTML = renderElements(characters);
---
+++ profiles.js
@@ -0,0 +1,30 @@
+const characters = [
+    {
+        name: "Gumi",
+        spriteLink: "Sprites/GumiSprite.png",
+        characterImageLink: "Characters/Gumi.png",
+        description: "Gumi is a fiery daredevil and scavenger who enjoys hunting for treasure and gambling.",
+        species: "Syrian Hamster",
+        age: "Young Adult",
+        gender: "Female",
+        habitat: "Nom's Hollow",
+        occupation: "Scavenger",
+        personality: "Energetic, hot-tempered, strong-willed, adventurous, and outgoing but also kind and reliable",
+        background: "Gumi's burrow was originally in an area that was overrun by beasts and all of her loot and treasured belongings were lost. As a result, she had to find a new place to keep her stash safe, and that’s how she found Nom’s Hollow. Along the way, she made some friends and learned to share the ‘glory of the loot’ with her friends."
+    },
+    {
+        name: "",
+        spriteLink: "",
+        characterImageLink: "",
+        description: "",
+        species: "",
+        age: "",
+        gender: "",
+        habitat: "",
+        occupation: "",
+        personality: "",
+        background: ""
+    },
+
+
+]
