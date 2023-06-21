f3de10ceae2a446fad8efda84a445414aa4c9ff0
Lindsey Steiner <105761115+Razetto@users.noreply.github.com>
2023-06-21 09:17:16 -0400

    Updated HTML and JS

--- index.js
+++ index.js
@@ -17,5 +17,49 @@ const renderElements = (elementsArray) => {
 
 sprites.innerHTML = renderElements(characters);
 
-const bios = document.querySelector(".bio-container");
+//
 
+const bios = document.querySelector(".bio-section");
+
+const renderSingleBio = (bio) => {
+    const {
+      name,
+      characterImageLink,
+      description,
+      species,
+      age,
+      gender,
+      habitat,
+      occupation,
+      personality,
+      background,
+    } = bio;
+  
+    return `
+    <div class="bio-section">
+        <div class="bio-container">
+            <div class="bio-image">
+            <img src="${characterImageLink}" alt="${name}" />
+            </div>
+            <div class="bio-text">
+            <h3>${name}</h3>
+            <p><strong>Description</strong>: ${description}</p>
+            <p><strong>Species</strong>: ${species}</p>
+            <p><strong>Age</strong>: ${age}</p>
+            <p><strong>Gender</strong>: ${gender}</p>
+            <p><strong>Habitat</strong>: ${habitat}</p>
+            <p><strong>Occupation</strong>: ${occupation}</p>
+            <p><strong>Personality</strong>: ${personality}</p>
+            <p><strong>Background</strong>: ${background}</p>
+            </div>
+        </div>
+    </div>
+    `;
+  };
+  
+  const renderBios = (biosArray) => {
+    const transformedBios = biosArray.map((bio) => renderSingleBio(bio));
+    return transformedBios.join("");
+  };
+  
+  bios.innerHTML = renderBios(characters);
